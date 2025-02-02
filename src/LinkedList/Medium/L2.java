bc8e2468f1d53db9a1522f37b59cb33dbe4e7328
Prakash Singh <prakashsingh@Prakashs-MacBook-Air.local>
2025-02-03 02:01:06 +0530

    Add two numbers - LinkedList

---
+++ src/LinkedList/Medium/L2.java
@@ -0,0 +1,61 @@
+package src.LinkedList.Medium;
+
+// 2. Add Two Numbers
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class L2 {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        ListNode dummy = new ListNode(0);
+        ListNode start = dummy;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            if (l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            dummy.next = new ListNode(sum % 10);
+            dummy = dummy.next;
+            carry = sum / 10;
+        }
+        if (carry != 0)
+            dummy.next = new ListNode(carry);
+        return start.next;
+    }
+
+    private void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + "->");
+            node = node.next;
+        }
+        System.out.print("NULL");
+    }
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+        L2 l = new L2();
+        ListNode finalList = l.addTwoNumbers(l1, l2);
+        l.printList(finalList);
+    }
+}
