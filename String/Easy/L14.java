54097ce5257c540739429f3d63da861374de2545
prakash12335 <prakash141994@gmail.com>
2025-02-08 18:52:09 +0530

    Longest Common Prefix - String

---
+++ String/Easy/L14.java
@@ -0,0 +1,30 @@
+package String.Easy;
+
+// 14. Longest Common Prefix
+
+import java.util.Arrays;
+
+public class L14 {
+
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length;
+        if (n == 0) return "";
+        Arrays.sort(strs);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < strs[0].length(); i++) {
+            if (strs[0].charAt(i) != strs[n - 1].charAt(i)) {
+                return sb.toString();
+            }
+            sb.append(strs[0].charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"flower", "flow", "flight"};
+        System.out.println(new L14().longestCommonPrefix(strs));
+    }
+}
+
+// TimeComplexity - O(N * logN)
+// TimeComplexity - O(1)
