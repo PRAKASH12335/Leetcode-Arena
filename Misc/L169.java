b47803e62319cfacde034a2865b54c6af2407234
prakash12335 <prakash141994@gmail.com>
2025-02-10 19:52:30 +0530

    Majority Element

---
+++ Misc/L169.java
@@ -0,0 +1,27 @@
+package Misc;
+
+// 169. Majority Element
+
+public class L169 {
+
+    public int majorityElement(int[] nums) {
+        int maj_ele = -1, count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                maj_ele = nums[i];
+                count = 1;
+            } else {
+                count += maj_ele == nums[i] ? 1 : -1;
+            }
+        }
+        return maj_ele;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+        System.out.println(new L169().majorityElement(nums));
+    }
+}
+
+// Time Complexity - O(N)
+// Time Complexity - O(1)
