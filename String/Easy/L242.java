5e7532514deffa64af8e23413a9e62f60f911254
prakash12335 <prakash141994@gmail.com>
2025-02-14 00:35:10 +0530

    Valid Anagram - String

---
+++ String/Easy/L242.java
@@ -0,0 +1,28 @@
+package String.Easy;
+
+// 242. Valid Anagram
+
+public class L242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0)
+                return false;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String s = "anagram", t = "nagaram";
+        System.out.println(new L242().isAnagram(s, t));
+    }
+}
+
+// Time Complexity - O(N)
+// Time Complexity - O(1)
