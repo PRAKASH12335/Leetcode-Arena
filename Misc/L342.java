cb1259bd32c232747213720d5504cc1147eb4f5e
prakash12335 <prakash141994@gmail.com>
2025-02-20 00:13:17 +0530

    Power of Four

---
+++ Misc/L342.java
@@ -0,0 +1,22 @@
+package Misc;
+
+// 342. Power of Four
+
+public class L342 {
+
+    public boolean isPowerOfFour(int n) {
+        int i = 1;
+        while (i < n) {
+            i = i << 2;
+        }
+        return i == n;
+    }
+
+    public static void main(String[] args) {
+        int n = 15;
+        System.out.print(new L342().isPowerOfFour(n));
+    }
+}
+
+// Time Complexity - O(logN)
+// Time Complexity - O(1)
