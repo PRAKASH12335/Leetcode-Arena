256b6352059ebb11c209b9e09fe3e12ac2783609
prakash12335 <prakash141994@gmail.com>
2025-03-01 16:16:29 +0530

    Integer limit fix - Interval

--- Interval/Medium/L452.java
+++ Interval/Medium/L452.java
@@ -6,9 +6,7 @@ import java.util.Arrays;
 
 public class L452 {
     public int findMinArrowShots(int[][] points) {
-        if (points.length == 0)
-            return 0;
-        Arrays.sort(points, (a, b) -> a[1] - b[1]);
+        Arrays.sort(points, (a, b) -> Integer.compare(a[1], b[1]));
         int end = points[0][1], count = 1;
         for (int i = 1; i < points.length; i++) {
             if (end < points[i][0]) {
