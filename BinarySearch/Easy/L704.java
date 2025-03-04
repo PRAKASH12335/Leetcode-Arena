fbd066f89271808b8815931c7ac13ee55f79f546
prakash12335 <prakash141994@gmail.com>
2025-03-04 10:32:39 +0530

    Binary Search

---
+++ BinarySearch/Easy/L704.java
@@ -0,0 +1,29 @@
+package BinarySearch.Easy;
+
+// 704. Binary Search
+
+public class L704 {
+
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target)
+                return mid;
+            if (nums[mid] > target)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+        System.out.println(new L704().search(nums, target));
+    }
+}
+
+// Time Complexity - O(log N)
+// Space Complexity - O(1)
