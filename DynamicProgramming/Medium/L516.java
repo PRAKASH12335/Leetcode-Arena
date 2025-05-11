d7f8f736c1d69b9a9527d7280a935a6b2bd4a662
prakash12335 <prakash141994@gmail.com>
2025-05-12 01:32:05 +0530

    Longest Palindromic Subsequence - DP

---
+++ DynamicProgramming/Medium/L516.java
@@ -0,0 +1,38 @@
+package DynamicProgramming.Medium;
+
+// 516. Longest Palindromic Subsequence
+
+public class L516 {
+    public String reverse(String s) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            sb.append(s.charAt(i));
+        }
+        return sb.toString();
+    }
+
+    public int longestPalindromeSubseq(String s, String t) {
+        int m = s.length(), n = t.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s = "bbbab";
+        L516 obj = new L516();
+        System.out.println(obj.longestPalindromeSubseq(s, obj.reverse(s)));
+    }
+}
+
+// Time Complexity - O(N*M)
+// Space Complexity - O(N*M)
