f3d25d9878a7b5463d9327a72f6fc207d37278b6
prakash12335 <prakash141994@gmail.com>
2025-05-11 11:48:48 +0530

    Subset Sum Equals to Target - DP

--- Striver/SubsetSumEqualstoTarget.java
+++ Striver/SubsetSumEqualstoTarget.java
@@ -50,7 +50,7 @@ public class SubsetSumEqualstoTarget {
         for (int i = 0; i < nums.length; i++) {
             dp[i][0] = true;
         }
-        dp[0][nums[0]] = true;
+        if (nums[0] < target) dp[0][nums[0]] = true;
         for (int i = 1; i < nums.length; i++) {
             for (int j = 1; j <= target; j++) {
                 boolean notTake = dp[i - 1][j];
