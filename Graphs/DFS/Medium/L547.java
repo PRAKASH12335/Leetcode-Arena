91fd14cafa0b8d66331e89aa5203a9d1eba31ebc
prakash12335 <prakash141994@gmail.com>
2025-05-19 21:11:56 +0530

    Number of Provinces using Disjoint Set - Graphs

--- Graphs/DFS/Medium/L547.java
+++ Graphs/DFS/Medium/L547.java
@@ -5,6 +5,56 @@ package Graphs.DFS.Medium;
 import java.util.ArrayList;
 import java.util.List;
 
+class DisjointSet {
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+
+    DisjointSet(int n) {
+        for (int i = 0; i <= n; i++) {
+            parent.add(i);
+            rank.add(0);
+            size.add(1);
+        }
+    }
+
+    public int findParent(int u) {
+        if (u == parent.get(u))
+            return u;
+        int ulp_u = findParent(parent.get(u));
+        parent.set(u, ulp_u);
+        return parent.get(u);
+    }
+
+    public void unionByRank(int u, int v) {
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if (ulp_u == ulp_v) return;
+        if (rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+        } else if (rank.get(ulp_u) > rank.get(ulp_v)) {
+            parent.set(ulp_v, ulp_u);
+        } else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
+        }
+    }
+
+    public void unionBySize(int u, int v) {
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if (ulp_u == ulp_v) return;
+        if (size.get(ulp_u) < size.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
+            size.set(ulp_v, size.get(ulp_u) + size.get(ulp_v));
+        } else {
+            parent.set(ulp_v, ulp_u);
+            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        }
+    }
+}
+
 public class L547 {
     private void dfs(int node, boolean[] vis, List<List<Integer>> adjList) {
         vis[node] = true;
@@ -44,6 +94,23 @@ public class L547 {
     public static void main(String[] args) {
         int[][] isConnected = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
         System.out.println(new L547().findCircleNum(isConnected));
+
+        int n = isConnected.length;
+        DisjointSet ds = new DisjointSet(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1) {
+                    ds.unionBySize(i + 1, j + 1);
+                }
+            }
+        }
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i == ds.findParent(i)) {
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
     }
 }
 
