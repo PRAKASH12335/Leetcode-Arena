1f777bf0adbb0b3e8b86ef3b10adc6db6ba3bb5f
prakash12335 <prakash141994@gmail.com>
2026-07-05 01:25:31 +0530

    Insertion Sort(Striver's Algo) - Sorting Algo

--- SortingAlgo/InsertionSort.java
+++ SortingAlgo/InsertionSort.java
@@ -5,6 +5,12 @@ package SortingAlgo;
 import java.util.Arrays;
 
 public class InsertionSort {
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
     private void insertionSort(int[] nums) {
         int n = nums.length;
         for (int i = 1; i < n; i++) {
@@ -21,8 +27,20 @@ public class InsertionSort {
     public static void main(String[] args) {
         int[] nums = {2, 7, 4, 1, 5, 3};
         InsertionSort is = new InsertionSort();
-        is.insertionSort(nums);
+        is.insertionSorting(nums);
         Arrays.stream(nums).forEach(a -> System.out.println(a));
+    }
+
+    // Striver's Algo
+    private void insertionSorting(int[] nums) {
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            int j = i;
+            while (j > 0 && nums[j] < nums[j - 1]) {
+                swap(j, j - 1, nums);
+                j--;
+            }
+        }
     }
 }
 
