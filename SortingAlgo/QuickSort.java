b911fd25afd2f2728e0fa55b21c30e3d9c3f1860
prakash12335 <prakash141994@gmail.com>
2026-07-05 11:37:57 +0530

    Quick Sort(Striver's Algo) - Sorting Algo

--- SortingAlgo/QuickSort.java
+++ SortingAlgo/QuickSort.java
@@ -5,12 +5,6 @@ package SortingAlgo;
 import java.util.Arrays;
 
 public class QuickSort {
-    private void swap(int i, int j, int[] nums) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
     private int partition(int[] nums, int start, int end) {
         int pivot = nums[end];
         int pIndex = start;
@@ -34,11 +28,47 @@ public class QuickSort {
 
     public static void main(String[] args) {
         int[] nums = {2, 7, 4, 1, 5, 3};
+        int[] nums1 = {2, 7, 4, 1, 5, 3};
         QuickSort qs = new QuickSort();
         qs.quickSort(nums, 0, nums.length - 1);
-        Arrays.stream(nums).forEach(a -> System.out.println(a));
+        qs.quickSorting(nums1, 0, nums.length - 1);
+        Arrays.stream(nums).forEach(a -> System.out.print(a + " "));
+        System.out.println();
+        Arrays.stream(nums1).forEach(a -> System.out.print(a + " "));
+    }
+
+    // Striver's Quick Sort
+    private void quickSorting(int[] nums, int low, int high) {
+        if (low < high) {
+            int pIndex = partitionIndex(nums, low, high);
+            quickSorting(nums, low, pIndex - 1);
+            quickSorting(nums, pIndex + 1, high);
+        }
+    }
+
+    private int partitionIndex(int[] nums, int low, int high) {
+        int pivot = nums[low];
+        int i = low, j = high;
+        while (i < j) {
+            while (i <= high - 1 && pivot >= nums[i]) {
+                i++;
+            }
+            while (j >= low + 1 && pivot < nums[j]) {
+                j--;
+            }
+            if (i < j)
+                swap(i, j, nums);
+        }
+        swap(j, low, nums);
+        return j;
+    }
+
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
 
-// Time Complexity - O(N * N)
-// Space Complexity - O(N)
+// Time Complexity - O(N * logN)
+// Space Complexity - O(1)
