7c06e5f1cbfc61d65104d38c339ab3c9fcb2e65e
prakash12335 <prakash141994@gmail.com>
2026-07-11 20:11:52 +0530

    Longest Subarray with given Sum K(Positives) - Arrays

--- Striver/Arrays/LongestSubArrayWithSumK.java
+++ Striver/Arrays/LongestSubArrayWithSumK.java
@@ -2,6 +2,9 @@ package Striver.Arrays;
 
 // Longest Subarray with given Sum K(Positives)
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LongestSubArrayWithSumK {
     public int longestSubarray(int[] nums, int k) {
         int start = 0, maxLength = -1, sum = 0;
@@ -20,12 +23,35 @@ public class LongestSubArrayWithSumK {
         return maxLength;
     }
 
+    // Time Complexity - O(N)
+    // Space Complexity - O(1)
+
+    public int longestSubarrayNegatives(int[] nums, int k) {
+        int n = nums.length;
+        int sum = 0, maxLen = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if (sum == k)
+                maxLen = Math.max(maxLen, i + 1);
+            if (map.containsKey(sum - k))
+                maxLen = Math.max(maxLen, i - map.get(sum - k));
+            if (!map.containsKey(sum))
+                map.put(sum, i);
+        }
+        return maxLen;
+    }
+
+    // Time Complexity - O(N)
+    // Space Complexity - O(N)
+
     public static void main(String[] args) {
         int[] nums = {10, 5, 2, 7, 1, 9};
         int k = 15;
-        System.out.println(new LongestSubArrayWithSumK().longestSubarray(nums, k));
+        LongestSubArrayWithSumK obj = new LongestSubArrayWithSumK();
+        System.out.println(obj.longestSubarray(nums, k));
+        int[] nums1 = {10, 5, 2, 7, 1, -10};
+        System.out.println(obj.longestSubarrayNegatives(nums1, k));
     }
 }
-
-// Time Complexity - O(N)
-// Time Complexity - O(1)
