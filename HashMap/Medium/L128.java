849358b99168e997de395a8484376fb2eefcb20d
prakash12335 <prakash141994@gmail.com>
2026-07-13 17:05:05 +0530

    Longest Consecutive Sequence - Arrays

--- HashMap/Medium/L128.java
+++ HashMap/Medium/L128.java
@@ -2,9 +2,28 @@ package HashMap.Medium;
 
 // 128. Longest Consecutive Sequence
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class L128 {
+    public int longestConsecutiveBetter(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int curr = 0, maxLength = 0, lastSmaller = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] - 1 == lastSmaller) {
+                curr++;
+                lastSmaller = nums[i];
+            } else if (nums[i] != lastSmaller) {
+                curr = 1;
+                lastSmaller = nums[i];
+            }
+            maxLength = Math.max(maxLength, curr);
+        }
+        return maxLength;
+    }
+    // Time complexity - O(N*logN + N)
+    // Space complexity - O(1)
 
     public int longestConsecutive(int[] nums) {
         HashSet<Integer> set = new HashSet<>();
@@ -25,11 +44,13 @@ public class L128 {
         return maxLength;
     }
 
+    // Time complexity - O(N)
+    // Space complexity - O(N)
+
     public static void main(String[] args) {
         int[] nums = {100, 4, 200, 1, 3, 2};
-        System.out.print(new L128().longestConsecutive(nums));
+        L128 obj = new L128();
+        System.out.println(obj.longestConsecutiveBetter(nums));
+        System.out.print(obj.longestConsecutive(nums));
     }
 }
-
-// Time complexity - O(N)
-// Space complexity - O(N)
